1122e3dc6cf12f4bde19516e00ba0d518ed48b26
paulyang1003 <paulyang1003@gmail.com>
2023-05-17 13:59:24 +0800

    no message

---
+++ src/main/java/web/clinic/clinicMember/entity/ClinicMember.java
@@ -0,0 +1,9 @@
+package web.clinic.clinicMember.entity;
+
+import core.pojo.Core;
+
+public class ClinicMember extends Core{
+	private Integer clinicId;
+	private String clinicName;
+	private String clinicPrinciple
+}
