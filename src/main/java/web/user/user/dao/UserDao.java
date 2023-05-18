843cd87af8ef796d2cca76e57aa7d686685c6220
Wade <wl02014258@gmail.com>
2023-05-18 18:52:55 +0800

    新增: UseDAO實作

--- src/main/java/web/user/user/dao/UserDao.java
+++ src/main/java/web/user/user/dao/UserDao.java
@@ -1,5 +1,12 @@
 package web.user.user.dao;
 
-public class UserDao {
+import core.dao.CoreDao;
+import web.user.user.entity.User;
+
+public interface UserDao extends CoreDao<User, Integer> {
+
+	User selectByAccount(String useraccount);
+
+	User selectForLogin(String useraccount, String userpassword);
 
 }
