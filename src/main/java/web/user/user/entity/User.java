da7fcbcf128cabf392ff1c2037345dfc9510b1bd
Wade <wl02014258@gmail.com>
2023-05-18 18:30:00 +0800

    新增: entity內容

--- src/main/java/web/user/user/entity/User.java
+++ src/main/java/web/user/user/entity/User.java
@@ -1,5 +1,47 @@
 package web.user.user.entity;
 
-public class User {
+import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import core.pojo.Core;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends Core {
+	/**
+	 * 會員
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Integer userId;
+    private String userAccount;
+    private String userPassword;
+    private String userName;
+    private String userGender;
+    private Date userBirth;
+    private String userTel;
+    private String userAddr;
+    private String userEmail;
+    private Byte userPic;
+    private String userRep;
+    private String userBan;
+    private Date userCreateTime;
+    private Date userUpdateTime;
+    private String userStatus;
 }
